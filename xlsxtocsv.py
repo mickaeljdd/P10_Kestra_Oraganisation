06623b8b2cf83f6414164d8148507913096587f1
Mickael Darmon <mickael.darmon@live.fr>
2026-05-17 16:35:57 +0200

    Ajout d'un convertisseur de xlsx vers csv (Aide à la lecture pour le flow)

---
+++ xlsxtocsv.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+def convert_excel_to_csv(excel_file, csv_file):
+    read_file = pd.read_excel(excel_file)
+    read_file.to_csv(csv_file, index=None, header=True)
+    df = pd.DataFrame(pd.read_csv(csv_file))
+if __name__ == "__main__":
+    add_rep = "./input/"
+    list_of_excel_files = ["Fichier_erp.xlsx", "fichier_liaison.xlsx", "Fichier_web.xlsx"]
+    list_of_csv_files = ["Fichier_erp.csv", "fichier_liaison.csv", "Fichier_web.csv"]
+    for excel_file, csv_file in zip(list_of_excel_files, list_of_csv_files):
+        print(add_rep + excel_file)
+        print(add_rep + csv_file)
+        convert_excel_to_csv(add_rep + excel_file, add_rep + csv_file)
